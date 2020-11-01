1bf8718683ff35523354f2f827d40d589feabb46
yuziroppe <syani4732@gmail.com>
2020-11-01 11:13:37 +0900

    直した

--- FollowUser/index.js
+++ FollowUser/index.js
@@ -1,33 +1,34 @@
 module.exports = async function (context, req) {
     context.log(`x-ms-client-principal-name: ${req.headers['x-ms-client-principal-name']}`);
 
+    if (req.headers['x-ms-client-principal-name'] == null) {
+        context.res = {
+            status: 403,
+        };
+        return;
+    }
+
+    if (req.body == null || req.body.follow_id == null) {
+        context.res = {
+            status: 400,
+            body: `missing follow id`,
+        };
+        return;
+    }
+
+    console.log(req.headers)
     const user_id = req.headers['x-ms-client-principal-name'];
+    const timestamp = Date.now();
+    const follow_id = req.body.follow_id;
 
-    if (user_id == null && (req.body == null || req.body.id == null)) {
-        context.res = {
-            status: 400,
-            body: `login or enter id`,
-        };
-        return;
+    context.bindings.outputDocument = {
+        id: `${user_id}_${follow_id}`,
+        user_id,
+        follow_id,
+        timestamp
     }
-
-    if (req.body == null || req.body.text == null) {
-        context.res = {
-            status: 400,
-            body: `missing text`,
-        };
-        return;
-    }
-
-    
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: 201,
     };
 }
